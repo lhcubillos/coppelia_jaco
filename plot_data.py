3d45ca943f00079096ac25300a2410db2662e909
ndeepak-umich <ndeepak@umich.edu>
2022-11-07 12:02:12 -0500

    adjust prints

--- plot_data.py
+++ plot_data.py
@@ -6,7 +6,7 @@ class PlotData:
     def __init__(self, folderpath):
         self.file = None
         self.folder_path = folderpath
-        self.patient_id = ['']
+        self.patient_id = ['natalia', 'anna', 'qi']
         self.string_to_match = ['pre_test', 'training_1_1', 'training_1_2', 'training_1_3', 'training_1_4', 'mid_test', 
         'training_2_1', 'training_2_2', 'training_2_3', 'training_2_4', 'post_test']
         self.negative_key = '_settings.tdt'
@@ -21,12 +21,14 @@ class PlotData:
     def start(self):
         fig = go.Figure()
 
-        
+        print("Test Name (X-axis) | Average Time for test (Y-axis) | Total Targets")
+        print("--------------------------------------------------------------------------------")
         for patient in self.patient_id:
+            print("\nPATIENT ID: {}".format(patient))
+            print("-------------------------------------------------------")
             for key_string in self.string_to_match:
-                for filename in os.listdir(self.folder_path + '/' + patient):
+                for filename in os.listdir(self.folder_path + patient):
                     if (key_string in filename) and (self.negative_key not in filename):
-                        print("processing file: {}".format(filename))
                         self.file = open(self.folder_path + '/' + patient + '/' + filename, newline='')
                         lines = csv.reader(self.file, delimiter='\t')
                         time_taken = 0.0
@@ -42,14 +44,17 @@ class PlotData:
 
                         self.average /= self.target_count
                         self.time_taken.append(self.average)
-                        print("time_taken: {}, target_count: {}".format(self.average, self.target_count))
+                        if ((key_string == 'pre_test') or (key_string == 'mid_test') or (key_string == 'post_test')):
+                            print("{}\t| {}\t| {}".format(key_string, self.average, self.target_count))
+                        else:
+                            print("{}\t| {}\t| {}".format(key_string, self.average, self.target_count))
                         self.file.close()
 
                         self.start_time = 0.0
                         self.target_count = 0
                         self.average = 0.0
-
             fig.add_trace(go.Scatter(x=self.string_to_match, y=self.time_taken, name=patient))
+            self.time_taken = []
 
         fig.update_layout(hovermode='x unified')
         fig.show()
