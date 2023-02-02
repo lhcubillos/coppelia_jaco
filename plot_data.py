02adcc6e9d1b76db6971b92ecf31daeb4b9f4a3c
NagallaDeepak <nagalla.deepak@gmail.com>
2023-02-02 20:28:01 +0530

    fix bug in plot data

--- plot_data.py
+++ plot_data.py
@@ -7,7 +7,7 @@ class PlotData:
     def __init__(self, folderpath):
         self.file = None
         self.folder_path = folderpath
-        self.patient_id = ['AG121222', 'AS121222', 'HZ121222', 'JM121422']
+        self.patient_id = ['DC013123']
         self.string_to_match = ['pre_test', 'training_1_1', 'training_1_2', 'training_1_3', 'training_1_4', 'mid_test', 
         'training_2_1', 'training_2_2', 'training_2_3', 'training_2_4', 'post_test']
         self.negative_key = '_settings.tdt'
@@ -48,7 +48,19 @@ class PlotData:
                     if (key_string in filename) and (self.negative_key not in filename):
                         self.file = open(self.folder_path + '/' + patient + '/' + filename, newline='')
                         lines = csv.reader(self.file, delimiter='\t')
+                        # time
+                        self.start_time = 0.0
+                        self.end_time = 0.0
+                        self.target_count = 0
+                        self.average = 0.0
                         time_taken = 0.0
+                        # path
+                        self.start_point = (0.0, 0.0, 0.0)
+                        self.prev_point = (0.0, 0.0, 0.0)
+                        self.short_distance = 0.0
+                        self.distance = 0.0
+                        self.target_reached = 1
+                        # self.normalized_dist_avg = 0.0
 
                         for line in lines:
                             # path
