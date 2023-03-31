cf003a97cb58a0ccdb198eb04a46ada842798e73
NagallaDeepak <nagalla.deepak@gmail.com>
2023-03-31 20:01:31 +0530

    modify code to ease the editing part in generating multiple graphs

--- plot_data.py
+++ plot_data.py
@@ -7,12 +7,10 @@ class PlotData:
     def __init__(self, folderpath):
         self.file = None
         self.folder_path = folderpath
-        self.patient_id = ['Patient/DC013123', 'Patient/WL020223', 'Patient/SH020323', 'Patient/CR020923', 'Patient/NJ020923', 'Patient/MG021323', 'Patient/BW021423', 'Patient/DD021723', 'Patient/JP030223']
-        '''  self.patient_id = ['Healthy/TN120622', 'Healthy/SO120622', 'Healthy/SO120722', 'Healthy/TA120722', 'Healthy/AS121222', 'Healthy/HZ121222', 'Healthy/AG121222', 'Healthy/TN121222', 'Healthy/MC121422', 'Healthy/JM121422', "Healthy/KR121422", "Healthy/RA122022", "Healthy/SR122022", "Healthy/LC122022", "Healthy/YC122122", "Healthy/MR122222", "Healthy/KR122222", "Healthy/EE011023", "Healthy/AM011023", "Healthy/M111111", "Healthy/A111111", "Healthy/N111111", "Healthy/S111111", "Healthy/Q111111"]'''
-        
-        
-        """self.patient_id = ["Healthy/RA122022", "Healthy/SR122022", "Healthy/LC122022", "Healthy/YC122122", "Healthy/MR122222", "Healthy/KR122222"]
-        self.patient_id = ['Healthy/TN120622', 'Healthy/SO120622', 'Healthy/SO120722', 'Healthy/TA120722', 'Healthy/AS121222', 'Healthy/HZ121222', 'Healthy/AG121222', 'Healthy/TN121222', 'Healthy/MC121422', 'Healthy/JM121422', "Healthy/KR121422", "Healthy/RA122022", "Healthy/SR122022", "Healthy/LC122022", "Healthy/M111111", "Healthy/A111111", "Healthy/N111111", "Healthy/S111111", "Healthy/Q111111"]"""
+        # self.patient_status = "patient"
+        # self.patient_id = ["DC013123", "WL020223", "SH020323", "CR020923", "NJ020923", "MG021323", "BW021423", "DD021723", "RM031023", "JP030223", "DM031723", "RW032323", "BC032723"]
+        self.patient_status = "healthy"
+        self.patient_id = ["CB031323", "PC031523", "TB031623", "RG031623", "JN032023"] #, "JE032023"]
         self.string_to_match = ['pre_test', 'training_1_1', 'training_1_2', 'training_1_3', 'training_1_4', 'mid_test', 
         'training_2_1', 'training_2_2', 'training_2_3', 'training_2_4', 'post_test']
         self.negative_key = '_settings.tdt'
@@ -32,10 +30,10 @@ class PlotData:
         self.normalized_dist_avg = 0.0
         self.avg_path = []
         self.header = ['patient'] + self.string_to_match
-        self.time_data_csv = open(folderpath + '/2d_time_data.csv', 'w')
+        self.time_data_csv = open(folderpath + '/' + self.patient_status + '_2d_time_data.csv', 'w')
         self.time_data_csv_writer = csv.writer(self.time_data_csv)
         self.time_data_csv_writer.writerow(self.header)
-        self.path_data_csv = open(folderpath + '/2d_path_data.csv', 'w')
+        self.path_data_csv = open(folderpath + '/' + self.patient_status + '_2d_path_data.csv', 'w')
         self.path_data_csv_writer = csv.writer(self.path_data_csv)
         self.path_data_csv_writer.writerow(self.header)
     
@@ -46,6 +44,7 @@ class PlotData:
         print("Test Name (X-axis) | Average Time for test (Y-axis - Fig1) | Average normalized path (Y-axis - Fig2) | Total Targets")
         print("--------------------------------------------------------------------------------")
         for patient in self.patient_id:
+            patient = self.patient_status + "/" + patient
             print("\nPATIENT ID: {}".format(patient))
             print("-------------------------------------------------------")
             for key_string in self.string_to_match:
